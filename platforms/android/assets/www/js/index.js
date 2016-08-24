c612fd8e1149c5d434278e6a7d8febfc7e368d69
ali <alim.rzaa@yahoo.com>
2016-08-25 00:50:25 +0430

    first commit

---
+++ platforms/android/assets/www/js/index.js
@@ -0,0 +1,357 @@
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+		pictureSource=navigator.camera.PictureSourceType;
+       destinationType=navigator.camera.DestinationType;
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+document.addEventListener('deviceready', onDeviceBase, false);
+document.addEventListener("offline", onOffline, false);
+document.addEventListener("online", onOnline, false);
+  function onOffline() {
+document.getElementById('online').value=0;
+}	
+  function onOnline() {
+document.getElementById('online').value=1;
+}	
+////////////////////////////////////
+function onDeviceBase() {
+var db = window.openDatabase("Database", "1.0", "Cordova borna", 200000);
+db.transaction(table, errorCB, successCB);
+}
+// end onDeviceBase
+
+function table(tx){    
+//tx.executeSql('DROP TABLE IF EXISTS cars');
+//tx.executeSql('DROP TABLE IF EXISTS settings');
+tx.executeSql('CREATE TABLE IF NOT EXISTS cars(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, id_car INTEGER, name text,comment text,bime text,pic text,direct text,company INTEGER,flag INTEGER,fav INTEGER)');
+tx.executeSql('CREATE TABLE IF NOT EXISTS settings(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, title text,valuem text)');
+//tx.executeSql('INSERT INTO settings(title,valuem) values("flag_one","1")');
+}
+
+///////////////////////////////////////error db
+function errorCB(err) {
+	console.log("Error processing SQLm: "+err.message);
+}
+///////////////////////////////////// هرچیزی که می خواهد در ابتدا استارت بخورد در این قسمت قرار بگیرد
+function successCB() {
+var db = window.openDatabase("Database", "1.0", "Cordova borna", 200000);
+db.transaction(flag_one, errorSE);
+}
+
+//////////////////////////////////// مرحله سنجش فلگ بار اول
+function flag_one(tx) {
+tx.executeSql('SELECT * FROM settings where title="last_car"', [], again_car, errorSE);
+}
+
+
+function errorSE() {  
+ // alert('kjh');
+$.getJSON("company.json", function(json) {
+var long=json.cars.length;		
+//alert(long);
+var dbs = window.openDatabase("Database", "1.0", "Cordova borna", 200000);
+dbs.transaction (function(tx){nenter_car(tx,long);},errOUT);	
+ 
+function nenter_car(tx,nter) {//alert('number'+nter);
+tx.executeSql('INSERT INTO settings(title,valuem) values("last_car",'+nter+')');
+}
+
+for(i = 0; i < json.cars.length; i++) {
+insert_cars(json.cars[i].ids, json.cars[i].name, json.cars[i].pic,json.cars[i].direct, json.cars[i].flag);
+}  
+
+
+function insert_cars(id,name,pic,direct,flag) {
+var dbs = window.openDatabase("Database", "1.0", "Cordova borna", 200000);
+dbs.transaction (function(tx){inserts_cars(tx,id,name,pic,direct,flag);}, errOUT);
+}
+
+function inserts_cars(tx,id,name,pic,direct,flag) {
+//alert(id+name+pic+direct);
+tx.executeSql('INSERT INTO cars(id_car,name,direct,pic,flag,fav) values('+id+', "'+name+'","'+direct+'", "'+pic+'",'+flag+',0)');
+}
+function succOUT() {
+  alert('best');
+}
+function errOUT(err) {
+  alert('eerr-'+err.message);
+}
+});
+
+///////////////////////// اپدیت  آیتم های قدیم
+
+function again_car(tx, results) {
+//alert('car');
+var counts=results.rows.item(0).valuem;
+//alert(counts);
+var x=0;
+
+$.getJSON("company.json", function(json) {
+for(i = counts; i < json.cars.length; i++) {
+x=x+1;
+insert_car(json.cars[i].ids, json.cars[i].name,json.cars[i].direct,  json.cars[i].comment, json.cars[i].flag);
+if(x==1){up_last_car(json.cars.length);}
+}
+for(i = 0; i < counts; i++) {
+	//alert(json.cars[i].name);
+up_flag_car(json.cars[i].ids, json.cars[i].name,json.cars[i].direct, json.cars[i].comment, json.cars[i].flag);
+}
+});
+}
+///////////////////////////////  ثبت آیتم های جدید
+function insert_car(id,name,pic,direct,flag) {
+var dbs = window.openDatabase("Database", "1.0", "Cordova borna", 200000);
+dbs.transaction (function(tx){insert_to_car(tx,id,name,pic,direct,flag);}, errorCB );	 
+}
+
+function insert_to_car(tx,id,name,pic,direct,flag) {
+tx.executeSql('INSERT INTO cars(ids,name,pic,direct,flag,fav) values('+id+', "'+name+'","'+pic+'","'+direct+'", '+flag+',0)');
+}
+
+}
+
+
+///////////////////////// اپدیت  آیتم های قدیم
+
+function up_flag_car(id,name,pic,bime,direct,company,comment,flag){
+var dbs = window.openDatabase("Database", "1.0", "Cordova borna", 200000);
+dbs.transaction (function(tx){up_function_car(tx,id,name,pic,bime,direct,company,comment,flag);}, testcard,errorCB );	 
+}
+
+function testcard() {
+}
+
+function up_function_car(tx,id,name,pic,bime,direct,company,comment,flag) {//alert(name);
+tx.executeSql("UPDATE cars SET name='"+name+"',comment='"+comment+"',company="+company+",pic='"+pic+"',bime='"+bime+"',direct='"+direct+"',flag="+flag+" where id_car="+id+"", [], testcard, errorCB );
+}
+////////// اپدیت تعداد ایتم های موجود در دیتا بیس محلی
+function up_last_car(number) {
+var dbs = window.openDatabase("Database", "1.0", "Cordova borna", 200000);
+dbs.transaction (function(tx){up_exe_car(tx,number);}, errorCB );	 
+}
+
+function up_exe_car(tx,number) {
+tx.executeSql("UPDATE settings SET valuem='" +number+"' where title='last_car'", [],errorCB );
+}
+
+
+////////////////////////////////////////////
+$( document ).on("swipeleft swiperight", ".ui-content", function( e ) {
+// We check if there is no open panel on the page because otherwise
+// a swipe to close the left panel would also open the right panel (and v.v.).
+// We do this by checking the data that the framework stores on the page element (panel: open).
+if ($.mobile.activePage.jqmData( "panel" ) !== "open") {
+if ( e.type === "swipeleft"  ) {
+$( "#myPanel" ).panel( "open" );
+$( "#myPanel2" ).panel( "open" );
+$( "#myPanel3" ).panel( "open" );
+} else if ( e.type === "swiperight" ) {
+$( "#myPanel" ).panel( "close" );
+$( "#myPanel2" ).panel( "close" );
+$( "#myPanel3" ).panel( "close" );
+}
+}
+});
+
+
+ 
+
+document.addEventListener("backbutton", function(e){
+       if($.mobile.activePage.is('#sherkat')){
+           e.preventDefault();
+           navigator.app.exitApp();
+       }
+       else if($.mobile.activePage.is('#mashinha')){
+  $.mobile.changePage( "#sherkat", { transition: "fade"} );
+       }
+       else {
+           navigator.app.backHistory()
+       }
+    }, false);
+
+function exitFromApp()
+{
+  navigator.app.exitApp();
+}
+//////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////
+// Called when a photo is successfully retrieved
+//
+function onPhotoURISuccessf(imageURI) {
+  var largeImage = document.getElementById('largeImage0');
+  largeImage.style.display = 'inline';
+  largeImage.src = imageURI;
+}
+// A button will call this function
+//
+function getPhotof(source) {
+
+  // Retrieve image file location from specified source
+  navigator.camera.getPicture(onPhotoURISuccessf, onFail, { quality: 50,
+	destinationType: destinationType.FILE_URI,
+	sourceType: source });
+}
+//////////////////////////////////////////////////////////////////////////////////////
+function onPhotoURISuccessy(imageURI) {
+  var largeImage = document.getElementById('largeImage1');
+  largeImage.style.display = 'inline';
+  largeImage.src = imageURI;
+}
+// A button will call this function
+//
+function getPhotoy(source) {
+  // Retrieve image file location from specified source
+  navigator.camera.getPicture(onPhotoURISuccessy, onFail, { quality: 50,
+	destinationType: destinationType.FILE_URI,
+	sourceType: source });
+}//
+/////////////////////////////////////////////////////////////////////////////////////
+function onPhotoURISuccessd(imageURI) {
+  var largeImage = document.getElementById('largeImage2');
+  largeImage.style.display = 'inline';
+  largeImage.src = imageURI;
+}
+// A button will call this function
+//
+function getPhotod(source) {
+  // Retrieve image file location from specified source
+  navigator.camera.getPicture(onPhotoURISuccessd, onFail, { quality: 50,
+	destinationType: destinationType.FILE_URI,
+	sourceType: source });
+}///////////////////////////////////////////////////////////////////////////////////////
+function onPhotoURISuccesss(imageURI) {
+  var largeImage = document.getElementById('largeImage3');
+  largeImage.style.display = 'inline';
+  largeImage.src = imageURI;
+}
+// A button will call this function
+//
+function getPhotos(source) {
+  // Retrieve image file location from specified source
+  navigator.camera.getPicture(onPhotoURISuccesss, onFail, { quality: 50,
+	destinationType: destinationType.FILE_URI,
+	sourceType: source });
+}
+// Called if something bad happens.
+///////////////////////////////////////////////////////////////////////////////////////
+function onFail(message) {
+  alert('خطا: ' + message);
+}
+
+///////////////////
+if (typeof jQuery == 'undefined') {
+  console.log("Notice :You Have To Add Jquery Befor Using This Library");
+}else{
+  $(document).on("click",".toast_material",function(){
+    if($('.toast_open').length){
+      $('.toast_material').removeClass("fadeInUp");
+      $('.toast_material').addClass("fadeOutDown");
+      setTimeout(function() {
+        $('.toast_material').remove();
+      }, 800);
+    }
+  });
+}
+function isset(variable) {
+    if (typeof variable !== "undefined" && variable !== null) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function Toast_Material(options){
+  if(isset(options.position)){
+    if(options.position == "right" || options.position == "left"){
+      var positionStyle = options.position + ":16px";
+    }
+    else if(options.position == "center"){
+      var positionStyle="";
+      $(document).ready(function(){
+        $('.toast_material').css({'left': '50%'});
+        $(".toast_material").css({'margin-left': -$(".toast_material").outerWidth() / 2 + 'px'});
+      });
+    }   
+  }
+  else{
+    var positionStyle = "left : 16px";
+  }
+  if(isset(options.align)){
+    var alignStyle = options.align;
+    if(alignStyle == "right"){
+      var directionStyle = "rtl";
+    }
+    else if(alignStyle == "left"){
+      var directionStyle = "ltr";
+    }
+    else if(alignStyle == "center"){
+      var directionStyle = "ltr";
+    }
+  }
+  else{
+    var alignStyle = "left";
+    var directionStyle = "ltr";
+  }
+  if(isset(options.content)){
+    var contentText = options.content;
+  }
+  else{
+    var contentText = "";
+  }
+  if(isset(options.updown)){
+    var heightStyle = options.updown + ":15px;";
+  }
+  else{
+    var heightStyle = "bottom : 15px";
+  }
+  if($('.toast_material').length){
+    if($('.toast_open').length){
+      $('.toast_material').addClass("animated fadeOutDown");
+    }
+    else{
+      $('.toast_material').remove();
+    } 
+    $('body').append('<div class="toast_material toast_open animated fadeInUp" style="'+ positionStyle +';text-align:'+ alignStyle +';'+ heightStyle +'"><p style="direction:'+ directionStyle +'">'+ contentText +'</p></div>');
+    setTimeout(function() {
+      $('.toast_material').removeClass("fadeInUp");
+      $('.toast_material').addClass("fadeOutDown");
+    }, 3000);
+  }
+  else{
+    console.log(heightStyle);
+    $('body').append('<div class="toast_material toast_open animated fadeInUp" style="'+ positionStyle +';text-align:'+ alignStyle +';'+ heightStyle +'"><p style="direction:'+ directionStyle +'">'+ contentText +'</p></div>');
+    setTimeout(function() {
+      $('.toast_material').removeClass("fadeInUp");
+      $('.toast_material').addClass("fadeOutDown");
+    }, 3000);
+  }
+};
+///////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ www/js/angularjs.js
@@ -0,0 +1,444 @@
+
+// JavaScript Document
+var App = angular.module('App', ['ngSanitize'] );
+
+App.controller('CenterCTRL', function ($scope,todoServicez,$http) {
+	
+$http.get("company.json").then(function(response) {
+	$scope.company = response.data.items;
+});
+
+$scope.comp_id = function (ides) {
+	//alert(ides);
+	$http.get("company.json").then(function(response) {
+	$scope.cars = response.data.cars;
+	  $scope.pageid=ides;
+});	
+}
+////////////////////////////////////////
+$scope.showfaver = function () {
+//faver  
+todoServicez.getfaver().then(function(items)
+{//alert(items[0].ids);
+	$scope.myfaver = items;
+});
+
+};
+$scope.faver = function (faver,id_var) 
+{
+	//alert(id_var);
+if(faver==1){
+ $scope.iconslike="img/likeoff.png";
+$scope.efaver=0;
+Toast_Material({ content : "با موفقیت از لیست علاقه مندی ها حذف شد", updown:"bottom", position:"center", align:"center" });	
+favt=0;}else{
+$scope.iconslike="img/heart.png";
+$scope.efaver=1;
+Toast_Material({ content : "با موفقیت در لیست علاقه مندی ها ثبت شد", updown:"bottom", position:"center", align:"center" });	
+favt=1;}
+todoServicez.faverat(id_var,favt);
+
+};
+///////////////////////////////////////////////////
+$scope.cars_id = function (ides) {
+var  ides;
+	$http.get("company.json").then(function(response) {
+	$scope.carm = response.data.cars;
+	$scope.carid=ides;	
+		 //alert($scope.carm[0].name);
+ 	$scope.picmi = response.data.pics;
+	$scope.caridp=ides;	
+var i;
+var src='';
+//alert($scope.carid);
+/////////////////////////////////////////////////////////////
+todoServicez.sherm(ides).then(function(items)
+{ 
+//alert(items[0].fav);
+$scope.efaver=items[0].fav;
+if(items[0].fav){
+$scope.efaver=items[0].fav;
+	$scope.iconslike="img/heart.png";
+	}else{
+		$scope.efaver=items[0].fav;
+		$scope.iconslike="img/likeoff.png";
+}
+
+	 
+});
+
+///////////////////////////////
+
+
+
+
+//////////////////////////////////////////////////////////// آیا فیور هست
+//todoServicez.iffav(ides).then(function(items)
+//{
+//
+//if(items[0].fav==1){
+//$scope.iconslike="img/heart.png";
+//	}else{
+//$scope.iconslike="img/likeoff.png";
+//	}
+//});
+///////////////////////////////////////////////////////////////////
+x=0;
+$('#owl-demo .five img').attr('src', src);
+$('#owl-demo .four img').attr('src', src);
+$('#owl-demo .three img').attr('src', src);
+$('#owl-demo .two img').attr('src', src);
+$('#owl-demo .one img').attr('src', src);
+////////////
+$('#myfive img').attr('src', src);
+$('#myfour img').attr('src', src);
+$('#mythree img').attr('src', src);
+$('#mytwo img').attr('src', src);
+$('#myone img').attr('src', src);
+ 
+for(i = 0; i < response.data.pics.length; i++) {
+if(response.data.pics[i].id_car==ides){//alert(response.data.pics[i].id_car);
+       src=response.data.pics[i].direct+response.data.pics[i].pic ;
+x=x+1;
+switch(x) {
+    case 1:
+$('#owl-demo .one img').attr('src', src);
+$('#myone img').attr('src', src);
+        break;
+    case 2:
+$('#owl-demo .two img').attr('src', src);
+$('#mytwo img').attr('src', src);
+        break;
+     case 3:
+$('#owl-demo .three img').attr('src', src);
+$('#mythree img').attr('src', src);
+        break;
+    case 4:
+$('#owl-demo .four img').attr('src', src);
+$('#owl-demo .five img').attr('src', src);
+$('#myfour img').attr('src', src);
+   
+        break;		
+    case 5:
+$('#owl-demo .five img').attr('src', src);
+$('#myfive img').attr('src', src);
+        break;			
+}
+}
+}
+	  });
+
+}
+
+///////////////////////////////////////////////////ersal form tamas
+$scope.forms = {};	
+$scope.forms.text="";
+$scope.forms.email="";
+$scope.forms.name="";
+
+ $scope.sendform = function () {
+	names=$scope.forms.name;
+	emails=$scope.forms.email;
+	texts=$scope.forms.text; 
+if(texts==""){
+Toast_Material({ content : "لطفا جهت ارسال پیام فیلد ها را کامل کنید", updown:"bottom", position:"center", align:"center" });	
+		return 0;}
+	
+  $http({
+  method  : 'POST',
+  url     : 'http://www.shahreroya.ir/apis/book/manage/code.php',
+  data    : $.param({name: names, email:emails, text:texts}),  // pass in data as strings
+  headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+ })
+.success(function(data) {
+$scope.forms.text="";
+$scope.forms.email="";
+$scope.forms.name="";
+Toast_Material({ content : "پیام شما با موفقیت به پشتیبانی ارسال شد", updown:"bottom", position:"center", align:"center" });	
+  });
+ }
+////////////////////////////////////////////////////////////////////////// 	
+$scope.randoms = Math.floor(Math.random() * 2) + 1  ;
+$scope.mylan = function (lan) {
+
+todoServicez.show_alert().then(function(items)
+{ 
+var len = items.length;
+
+for (var i=0; i<len; i++){
+//alert(items[i].valuem);
+todoServicez.up_alert(items[i].ids);
+}
+});
+ 
+}; 
+
+///////////////////////////////////////////////// ارسال تصویر
+ $scope.users = {};	
+$scope.sendform = function(urlpic) {
+online=document.getElementById('online').value;
+
+$scope.user.company='no';
+//var laImage = document.getElementById('largeImage0').src;	
+if(!$scope.user.company || !$scope.user.cars){
+
+Toast_Material({ content : "لطفا  نام خودرو را وارد کنید!", updown:"bottom", position:"center", align:"center" });	
+
+}else
+if(online==0){
+Toast_Material({ content : "اتصال به اینترنت برقرار نیست!", updown:"bottom", position:"center", align:"center" });	
+}else
+{	
+$scope.btshow=true;
+
+Toast_Material({ content : "برنامه در حال ارسال اطلاعات می باشد لطفا منتظر بمانید!", updown:"bottom", position:"center", align:"center" });	
+
+for(var i = 0; i < 4; i++){
+var d = new Date();	
+namefile=d.getTime()+'.jpg';
+var largeImage = document.getElementById('largeImage'+i);
+
+imageURI=largeImage.src;
+//alert(imageURI);
+if(i==3){ends='end'}else{ends='no'}
+todoServicez.UserImg(imageURI,namefile,ends).then(function(items)
+{
+//alert(items);
+if(items=='end' || items=='not'){
+$scope.btshow=false;	
+
+Toast_Material({ content : "ارسال به اتمام رسید!", updown:"bottom", position:"center", align:"center" });	
+}
+});
+ 
+  $http({
+  method  : 'POST',
+  url     : 'http://www.borna-grp.ir/sabt_kh.php',
+  data    : $.param({company: $scope.user.company, cars:$scope.user.cars, photo:namefile}),  // pass in data as strings
+  headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+ })
+  .success(function(data) {
+   // alert(data.items[0].cell);
+  });
+};
+}
+}
+/////////////////////////////////////////////////////////ورود به سیستم
+
+interd=document.getElementById('inter').value;
+online=document.getElementById('online').value;
+if(interd==1){
+navigator.notification.confirm("آیا می خواهید از برنامه خارج شوید؟ ", onConfirm, "خروج از برنامه!", "بله,خیر"); 
+    // Prompt the user with the choice
+function onConfirm(button) {
+    if(button==2){//If User selected No, then we just do nothing
+        return;
+    }else{
+        navigator.app.exitApp();// Otherwise we quit the app.
+  }
+}}else
+if(online==0 && interd==0){
+    $.mobile.changePage( "#online", { transition: "slideup"} );
+}
+$scope.go = function ( path ) {$location.path( path );};
+
+todoServicez.idreg().then(function(items)
+{
+	$scope.regiser = items;
+	if($scope.regiser){
+    $.mobile.changePage( "#sherkat", { transition: "slideup"} );
+	document.getElementById('inter').value=1;
+	return 0;
+	}
+});
+
+$scope.trustSrc = function(src) {
+return $sce.trustAsResourceUrl(src);
+}
+$scope.movie = {src:"http://www.borna-grp.ir/req.php", title:"واریز به حساب"};
+/////////////////////////////////////////////////////////////////////
+$scope.user = {};
+$scope.sabtcode = function() {
+online=document.getElementById('online').value;
+if(online==0 && interd==0){
+    $.mobile.changePage( "#online", { transition: "slideup"} );
+}
+
+var uid = device.uuid;
+
+  $http({
+  method  : 'POST',
+  url     : 'http://www.borna-grp.ir/sabt_kh.php',
+  data    : $.param({name: $scope.user.fname, mname:$scope.user.name,userid:uid, codes:$scope.user.code}),  // pass in data as strings
+  headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+ })
+  .success(function(data) {
+//alert(data.items[0].cell);
+if(data.items[0].cell!=''){
+todoServicez.insertcod(data.items[0].cell);
+    $.mobile.changePage( "#sherkat", { transition: "slideup"} );
+}else{
+alert('کد خرید وارد شده صحیح نمی باشد');
+}
+  });
+};
+
+
+});
+
+
+
+App.service('todoServicez', function($q) 
+{
+this.sherm = function(para)
+  {  //alert(para);
+   var idcom=para;
+	  var deferred, result = [];
+	  deferred = $q.defer();
+	  var db = window.openDatabase("Database", "1.0", "Cordova borna", 200000);
+	  db.transaction(function(tx) 
+	  { tx.executeSql("select * from cars where id_car="+idcom, [], function(tx, res) 
+		  { 
+			  for(var i = 0; i < res.rows.length; i++)
+			  {//alert(res.rows.item(i).fav);
+		  result.push({id : res.rows.item(i).id_car, name : res.rows.item(i).name, pic: res.rows.item(i).pic,fav : res.rows.item(i).fav, direct: res.rows.item(i).direct})
+		  }
+		  deferred.resolve(result);
+		});
+	  });
+	  return deferred.promise;
+    },
+this.iffav = function(para)
+{   var idcom=para;
+//alert(idcom);
+var deferred, result = [];
+deferred = $q.defer();
+var db = window.openDatabase("Database", "1.0", "Cordova borna", 200000);
+db.transaction(function(tx) 
+{ tx.executeSql("select fav from cars where id_car="+idcom, [], function(tx, res) 
+  { 
+	  for(var i = 0; i < res.rows.length; i++)
+	  {
+  result.push({fav : res.rows.item(i).fav})
+  }
+  deferred.resolve(result);
+});
+});
+return deferred.promise;
+},
+
+this.faverat = function(idss,fave) 
+    {//alert(idss+fave);
+		var db = window.openDatabase("Database", "1.0", "Cordova borna", 200000);
+        db.transaction(function(tx) 
+        {
+return tx.executeSql("UPDATE cars SET fav="+fave+" where id_car="+idss , [], function(tx, res) 
+            {
+                return true;
+            });
+        });
+        return false;
+    },
+this.UserImg=function(imageURI,file_name,counts){
+         	var deferred, result = [];
+            var deferred = $q.defer();
+			var options = new FileUploadOptions();
+			options.fileKey="file";
+			options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+			var params = {};
+			params.value1 = file_name;
+			params.value2  = counts;
+			options.params = params;
+			var ft = new FileTransfer();
+			ft.upload(imageURI, encodeURI('http://www.borna-grp.ir/sabt_kh.php'),
+				function(r){
+					//console.log("Code = " + r.responseCode);
+					//alert("Response = " + r.response);
+					//console.log("Sent = " + r.bytesSent);
+					deferred.resolve(r.response);
+
+				},
+				function(error){
+					//alert("An error has occurred: Code = " + error.code);
+					//console.error("upload error source " + error.source);
+					//console.error("upload error target " + error.target);
+					deferred.reject(error);
+
+				}, options);
+
+              return deferred.promise;
+               },	
+             this.idreg = function()
+						{  
+						var deferred, result = [];
+						deferred = $q.defer();
+						var db = window.openDatabase("Database", "1.0", "Cordova borna", 200000);
+						db.transaction(function(tx) 
+						{ tx.executeSql("SELECT * FROM settings where title='id_reg'", [], function(tx, res) 
+						{ 
+						result=res.rows.item(0).valuem;
+						deferred.resolve(result);
+						});
+						});
+						return deferred.promise;
+						},
+		this.insertcod = function(idss) 
+		{
+		var db = window.openDatabase("Database", "1.0", "Cordova borna", 200000);
+        db.transaction(function(tx) 
+        {
+            return tx.executeSql('INSERT INTO settings(title,valuem) values("id_reg","'+idss+'")' , [], function(tx, res) 
+            {
+                return true;
+            });
+        });
+        return false;
+    }		
+this.getfaver = function()
+  {   
+	  var deferred, result = [];
+	  deferred = $q.defer();
+	  var db = window.openDatabase("Database", "1.0", "Cordova borna", 200000);
+	  db.transaction(function(tx) 
+	  {
+	   tx.executeSql("select * from cars where fav=1", [], function(tx, res) 
+		  {
+		for(var i = 0; i < res.rows.length; i++)
+		{
+		  result.push({ids : res.rows.item(i).id_car, name : res.rows.item(i).name, pic: res.rows.item(i).pic,fav : res.rows.item(i).fav, direct: res.rows.item(i).direct})
+		}
+		  deferred.resolve(result);
+		});
+	  });
+	  return deferred.promise;
+    },
+this.show_alert = function()
+  {   
+	  var deferred, result = [];
+	  deferred = $q.defer();
+	  var db = window.openDatabase("Database", "1.0", "Cordova borna", 200000);
+	  db.transaction(function(tx) 
+	  { tx.executeSql("select * from settings where uptime=0 and flag=1", [], function(tx, res) 
+		  {
+		for(var i = 0; i < res.rows.length; i++)
+		{
+		  result.push({ids : res.rows.item(i).ids,title : res.rows.item(i).title, valuem : res.rows.item(i).valuem,flag : res.rows.item(i).flag, type : res.rows.item(i).type})
+		}
+		  deferred.resolve(result);
+		});
+	  });
+	  return deferred.promise;
+    },
+	this.up_alert = function(idss)
+  {  // alert(idss);
+	 var db = window.openDatabase("Database", "1.0", "Cordova borna", 200000);
+        db.transaction(function(tx) 
+        {
+            return tx.executeSql("UPDATE settings SET uptime=1 where ids="+idss , [], function(tx, res) 
+            {
+                return true;
+            });
+        });
+        return false;
+    }
+});
